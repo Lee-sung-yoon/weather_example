c3bef184413303b0a44d0abb6ec5f4eb8cd30c59
LEE <tjddbs1412@naver.com>
2023-02-27 10:39:55 +0900

    Initial commit

---
+++ src/test/java/com/example/weather_example/WeatherExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
