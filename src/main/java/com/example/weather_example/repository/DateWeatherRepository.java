24436042a8e40c15a8f9aebc70b78831649b3063
LEE <tjddbs1412@naver.com>
2023-02-28 12:21:22 +0900

    feat : [005-SPRING-SCHEDULER] Scheduler(cron) 이해
    change
    ---
    API에서 데이터 가져오는 방법과 Scheduler를 통해 과거 데이터를 저장하여 DB에서 데이터를 가져오는 방식을 구현

---
+++ src/main/java/com/example/weather_example/repository/DateWeatherRepository.java
@@ -0,0 +1,15 @@
+package com.example.weather_example.repository;
+
+import com.example.weather_example.domain.DateWeather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findAllByDate(LocalDate date);
+
+
+}
